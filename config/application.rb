f9c73f029bce63274c9cadb80920332b9f433ab3
Amir Mourali <amourali7135@gmail.com>
2019-10-28 02:47:40 +0800

    Routine push

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# require "attachinary/orm/active_record" # active_record or mongoid #added for attachinary
+
 
 
 # Require the gems listed in Gemfile, including any gems
