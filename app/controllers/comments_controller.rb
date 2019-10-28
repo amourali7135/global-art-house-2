be882cbc7a4005ae537b79a49f0adf8910fb21df
Amir Mourali <amourali7135@gmail.com>
2019-10-28 18:51:07 +0800

    No more nested art routes FML

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,37 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @comments = @art.comments.includes(:user)
   end
-    
+
     def new
       @comment = Comment.new
     end
-    
-    
+
+
     def create
       @art = Art.find(params[:art_id])
       @comment = @art.comments.create(comment_params)
       @comment.user_id = current_user.id
       if @comment.save
-        redirect_to @art
+        redirect_to artist_art_path(@art.artist, @art)
       else
         flash.now[:danger] = "error"
       end
     end
-    
+
     def destroy
       @comment = Comment.find(params[:id])
       @art = @comment.art
       @comment.destroy
       redirect_to @art
     end
-    
-    private 
-    
+
+    private
+
     def comment_params
       params.require(:comment).permit(:content, :art_id, :user_id)
     end
-    
+
   end
-  
