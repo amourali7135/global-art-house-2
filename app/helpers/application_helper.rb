c77bde0c5eb52d1e6486d25af5e13f94c461fcbd
Amir Mourali <amourali7135@gmail.com>
2019-11-15 04:25:30 +0800

    Artist show card social media buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
    include Pagy::Frontend
+
+   def link_to_image(image_path, target_link, image_options={}, options={})
+    link_to(image_tag(image_path, image_options), target_link, options)
+  end
+  
 end
