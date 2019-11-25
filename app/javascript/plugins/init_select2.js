93c181e778497536907631afbfe85185acfa19f0
Amir Mourali <amourali7135@gmail.com>
2019-11-25 14:41:28 +0800

    Select 2 reversion experiment for fucking heroku

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -1,11 +1,12 @@
-// import $ from 'jquery';
-// import 'select2';
-// import 'select2/dist/css/select2.min.css';
+import $ from 'jquery';
+ import 'select2';
+ import 'select2/dist/css/select2.min.css';
 
-// const initSelect2 = () => {
-//   console.log('hello')
-//   $("#artist_form").select2();
-// }
+ const initSelect2 = () => {
+   console.log('hello')
+   $("#search_tag_ids").select2();
+   $("#search_tag_ids").select2();
+ }
 
 
-// export { initSelect2 }
+ export { initSelect2 }
