a61c45610b5fc1117f89a292248b2adab8815e99
Amir Mourali <amourali7135@gmail.com>
2019-11-25 18:15:21 +0800

    Old migrations fucking heroku up should be gone

--- db/migrate/20191018072716_rename_artist_idsmh.rb
+++
@@ -1,5 +0,0 @@
-class RenameArtistIdsmh < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :art_tags, :artists_id, :artist_id
-  end
-end
--- db/migrate/20191018075331_drop_artist_tag_original_reference.rb
+++
@@ -1,5 +0,0 @@
-class DropArtistTagOriginalReference < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :art_tags, :artist_id
-  end
-end
