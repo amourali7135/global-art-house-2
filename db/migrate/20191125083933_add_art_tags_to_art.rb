0fa3e1145b47b11a9ab2e8ee833ccbc18704f555
Amir Mourali <amourali7135@gmail.com>
2019-11-25 16:52:18 +0800

    Art tag fix attempts

---
+++ db/migrate/20191125083933_add_art_tags_to_art.rb
@@ -0,0 +1,13 @@
+class AddArtTagsToArt < ActiveRecord::Migration[5.2]
+  def change
+    create_table :art_tags do |t|
+      t.references :art, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
+
+
+
