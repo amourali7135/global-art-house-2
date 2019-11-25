91dc3c89d23e62a9d0d20f91fa312133696b910d
Amir Mourali <amourali7135@gmail.com>
2019-11-25 16:08:15 +0800

    Preliminary select 2 conversions done

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -5,8 +5,26 @@ import $ from 'jquery';
  const initSelect2 = () => {
    console.log('hello')
    $("#search_tag_ids").select2();
-   $("#search_tag_ids").select2();
+   $("#browse_genre_tags").select2();
+   $("#artist_kinds_search").select2();
+   $("#artists_country_search").select2();
+   $("#artist_search_country").select2();
+   $("#artist_edit_tags").select2();
+   $("#artist_new_tags").select2();
+   $("#art_edit_kinds").select2();
+   $("#art_edit_types").select2();
+   $("#art_new_types").select2();
+   $("#art_new_kinds").select2();
+   $("#browse_country").select2();
    $("#search_country").select2();
+   $("#country_select").select2();
+   $("#country_select_again").select2();
+   $("#home_page_country_1").select2();
+   $("#home_page_country_2").select2();
+    // $("#search_tag_ids").select2({
+    //   multiple: true
+    // });
+
  }
 
 
